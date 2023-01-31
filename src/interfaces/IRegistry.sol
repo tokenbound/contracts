c02a00f122d69e68d561404f18f6c8ec7d47d76a
Jayden Windle <jaydenwindle@gmail.com>
2023-01-31 09:19:58 -0800

    fixed registry interface to match eip

--- src/interfaces/IRegistry.sol
+++ src/interfaces/IRegistry.sol
@@ -4,20 +4,16 @@ pragma solidity ^0.8.13;
 interface IRegistry {
     event AccountCreated(
         address account,
-        uint256 chainId,
         address tokenContract,
         uint256 tokenId
     );
 
-    function createAccount(
-        uint256 chainId,
-        address tokenContract,
-        uint256 tokenId
-    ) external returns (address);
+    function createAccount(address tokenContract, uint256 tokenId)
+        external
+        returns (address);
 
-    function account(
-        uint256 chainId,
-        address tokenContract,
-        uint256 tokenId
-    ) external view returns (address);
+    function account(address tokenContract, uint256 tokenId)
+        external
+        view
+        returns (address);
 }
