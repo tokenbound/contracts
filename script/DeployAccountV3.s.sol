c778aa02afaf018a9ba42b6843d8da0e24e9a534
Jayden Windle <jaydenwindle@gmail.com>
2023-10-11 22:06:02 -0700

    Return to main branch of erc6551 dependency

--- script/DeployAccountV3.s.sol
+++ script/DeployAccountV3.s.sol
@@ -10,8 +10,8 @@ contract DeployAccountV3 is Script {
     function run() external {
         bytes32 salt = 0x6551655165516551655165516551655165516551655165516551655165516551;
         address erc4337EntryPoint = 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789;
-        address multicallForwarder = 0x0000000000000000000000000000000000006551;
-        address erc6551Registry = 0x0000000000000000000000000000000000006551;
+        address multicallForwarder = 0xcA1167915584462449EE5b4Ea51c37fE81eCDCCD;
+        address erc6551Registry = 0x002c0c13181038780F552f0eC1B72e8C720147E6;
         address accountGuardian = 0x026FAa5A8212B68b65C523DCBEb158Cd7a6Ae09C;
 
         vm.startBroadcast();
